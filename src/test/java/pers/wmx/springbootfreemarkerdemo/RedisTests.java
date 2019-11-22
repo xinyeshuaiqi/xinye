6b8966642db3196e6c2ea723ca70c21e1d23a714
wang.wmx <wang.wmx@alibaba-inc.com>
2019-11-22 18:05:30 +0800

    update

--- src/test/java/pers/wmx/springbootfreemarkerdemo/RedisTests.java
+++ src/test/java/pers/wmx/springbootfreemarkerdemo/RedisTests.java
@@ -3,10 +3,16 @@ package pers.wmx.springbootfreemarkerdemo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * @author wmx
@@ -19,8 +25,38 @@ public class RedisTests {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
+    @Autowired
+    RedissonClient redissonClient;
+
     @Test
     public void testString(){
-        stringRedisTemplate.opsForValue().set("fpx","win");
+        stringRedisTemplate.opsForValue().set("haha","hehe");
+    }
+
+    @Test
+    public void testPipelined(){
+        List<Object> objects = stringRedisTemplate.executePipelined(new RedisCallback<Object>() {
+            @Override
+            public Object doInRedis(RedisConnection connection) throws DataAccessException {
+
+                connection.set("super carry".getBytes(), "doinb".getBytes());
+                connection.set("tian".getBytes(), "神僧".getBytes());
+
+                connection.get("super carry".getBytes());
+                connection.get("tian".getBytes());
+
+                return null;
+            }
+        });
+        System.out.println(objects);
+    }
+
+    @Test
+    public void testTransaction(){
+        stringRedisTemplate.setEnableTransactionSupport(true);
+        stringRedisTemplate.opsForValue().set("1","a");
+        stringRedisTemplate.opsForValue().set("2","b");
+        stringRedisTemplate.opsForValue().set("3","c");
     }
 }
+
