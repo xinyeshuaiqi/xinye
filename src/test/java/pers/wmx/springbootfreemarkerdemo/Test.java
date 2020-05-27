930071b3ae7e9237e5165feda60d526b95958a8d
wangmingxin03 <wangmingxin03@kuaishou.com>
2020-05-27 19:01:32 +0800

    test time format

--- src/test/java/pers/wmx/springbootfreemarkerdemo/Test.java
+++ src/test/java/pers/wmx/springbootfreemarkerdemo/Test.java
@@ -1,13 +1,34 @@
 package pers.wmx.springbootfreemarkerdemo;
 
+import static java.time.LocalDateTime.ofInstant;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * @author: wangmingxin03
  * @date: 2020-05-27
  */
 public class Test {
+    private static final DateTimeFormatter dateTimeFormatter =
+            DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss");
 
     public static void main(String[] args) {
-        //String.format(gameOutComeContentList.get(gameOutcome), authorData.getDeltaScore())
-        System.out.println(String.format("-%s",0));
+        long now = System.currentTimeMillis();
+
+        String result1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                .format(new Date(now));
+        System.out.println(result1);
+
+        System.out.println(timestampToDateStr(now));
+    }
+
+    private static String timestampToDateStr(long timestamp) {
+        LocalDateTime localDateTime = ofInstant(Instant.ofEpochMilli(timestamp), TimeZone.getDefault().toZoneId());
+        return dateTimeFormatter.format(localDateTime);
     }
 }
