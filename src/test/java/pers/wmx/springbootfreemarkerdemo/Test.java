b080d158067193c1bfbd7f7b4441cb589ce959d9
wangmingxin03 <wangmingxin03@kuaishou.com>
2020-05-27 16:59:19 +0800

    up

---
+++ src/test/java/pers/wmx/springbootfreemarkerdemo/Test.java
@@ -0,0 +1,13 @@
+package pers.wmx.springbootfreemarkerdemo;
+
+/**
+ * @author: wangmingxin03
+ * @date: 2020-05-27
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        //String.format(gameOutComeContentList.get(gameOutcome), authorData.getDeltaScore())
+        System.out.println(String.format("-%s",0));
+    }
+}
