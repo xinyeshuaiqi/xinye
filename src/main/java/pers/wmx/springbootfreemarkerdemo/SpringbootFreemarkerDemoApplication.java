5afd4dd0454e0f340d0101b95e931d554f636e5d
wangmingxin03 <wangmingxin03@kuaishou.com>
2020-07-08 14:37:54 +0800

    up

--- src/main/java/pers/wmx/springbootfreemarkerdemo/SpringbootFreemarkerDemoApplication.java
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/SpringbootFreemarkerDemoApplication.java
@@ -2,12 +2,19 @@ package pers.wmx.springbootfreemarkerdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootFreemarkerDemoApplication {
+public class SpringbootFreemarkerDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootFreemarkerDemoApplication.class, args);
     }
 
+    //为了打包springboot项目
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
