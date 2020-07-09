1a5be14ee10131a85a7b99afabd7c77535303809
wangmingxin03 <wangmingxin03@kuaishou.com>
2020-07-09 17:45:38 +0800

    red pack

--- src/test/java/pers/wmx/springbootfreemarkerdemo/redpack/RandomTest.java
+++ src/test/java/pers/wmx/springbootfreemarkerdemo/redpack/RandomTest.java
@@ -23,11 +23,6 @@ public class RandomTest {
     /**
      * 抢红包最重要的莫过于拆包
      * 拆的金额要保证尽可能随机，还要保证不能出现资损
-     * 还要包
-     *
-     * 为啥要  金额 / 红包数 * 2 我现在还没搞明白
-     *
-     * 可见 数学还是得好啊。。。
      *
      * 先抢后抢拿到红包的大小的期望是大致相等的，所以还是先下手抢吧
      * 后抢的人方差大（依赖前面人抢的多少），波动较大，有较大几率拿到手气最佳
@@ -43,14 +38,14 @@ public class RandomTest {
             return (double) Math.round(redPackage.remainMoney * 100) / 100;
         }
 
-        Random r     = new Random();
-        double min   = 0.01;
-        double max   = redPackage.remainMoney / redPackage.remainSize * 2;
-        double money = r.nextDouble() * max;    //13.252960381435447
+        Random r = new Random();
+        double min = 0.01;
+        double max = redPackage.remainMoney / redPackage.remainSize * 2;
+        double money = r.nextDouble() * max;
         money = money <= min ? 0.01: money;
-        money = Math.floor(money * 100) / 100;  //13.25
+        money = Math.floor(money * 100) / 100;
         redPackage.remainSize--;
-        redPackage.remainMoney -= money;  // 100 - 13.25 =86.75
+        redPackage.remainMoney -= money;
         return money;
     }
 
