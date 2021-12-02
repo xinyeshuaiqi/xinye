be3fdc4e7b3a644ecb33b403ed03c02b66abb526
wangmingxin03 <wangmingxin03@kuaishou.com>
2021-12-02 21:47:06 +0800

    rpc

---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/rpc/client/RpcClientMain.java
@@ -0,0 +1,19 @@
+package pers.wmx.springbootfreemarkerdemo.rpc.client;
+
+import pers.wmx.springbootfreemarkerdemo.rpc.server.SayHelloRpcService;
+
+/**
+ * @author wangmingxin03
+ * Created on 2021-12-02
+ */
+public class RpcClientMain {
+    public static void main(String[] args) throws Exception {
+        RpcClient client = new RpcClient();
+        SayHelloRpcService service = client
+                .callService(SayHelloRpcService.class, "127.0.0.1", 6666);
+
+        String request = "xinye";
+        String response = service.hello(request);
+        System.out.println(response);
+    }
+}
