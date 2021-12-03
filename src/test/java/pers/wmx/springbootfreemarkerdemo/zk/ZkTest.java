d564a60b678a03fe88ec8823814fb20fadc77ab5
wangmingxin03 <wangmingxin03@kuaishou.com>
2021-12-03 18:14:14 +0800

    update

--- src/test/java/pers/wmx/springbootfreemarkerdemo/zk/ZkTest.java
+++ src/test/java/pers/wmx/springbootfreemarkerdemo/zk/ZkTest.java
@@ -3,6 +3,7 @@ package pers.wmx.springbootfreemarkerdemo.zk;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,21 @@ public class ZkTest {
     @Test
     public void testInsert() {
         try {
-            zkClient.create("/hello", "zk".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            zkClient.create("/hello/hello1", "zk".getBytes(),
+                    ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
         } catch (Exception e) {
             log.error("create node fail ... ", e);
+        }
+    }
+
+    @Test
+    public void testQuery() {
+        try {
+            Stat stat = new Stat();
+            byte[] bytes = zkClient.getData("/hello", false, stat);
+            log.info("query result:{}", new String(bytes));
+        } catch (Exception e) {
+            log.error("query fail ... ", e);
         }
     }
 
