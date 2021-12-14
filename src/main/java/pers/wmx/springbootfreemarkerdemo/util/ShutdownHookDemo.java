545397ca55ede188b62a70d1788b5c002a94aae3
wangmingxin03 <wangmingxin03@kuaishou.com>
2021-12-14 15:35:12 +0800

    shut down

---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/util/ShutdownHookDemo.java
@@ -0,0 +1,22 @@
+package pers.wmx.springbootfreemarkerdemo.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author wangmingxin03
+ * Created on 2021-12-14
+ */
+@Slf4j
+public class ShutdownHookDemo {
+    private static class ShutDownHook extends Thread {
+        public void run() {
+            log.info("shut down ... ");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        log.info("start ... ");
+        Runtime.getRuntime().addShutdownHook(new ShutDownHook());
+        Thread.sleep(5000);
+    }
+}
