b8691e6b533f736b63be7d7970d65411c9fbbc1e
wangmingxin03 <wangmingxin03@kuaishou.com>
2021-12-20 21:44:51 +0800

    Partitioner

---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/kafka/SimplePartitionerKafkaProducer.java
@@ -0,0 +1,44 @@
+package pers.wmx.springbootfreemarkerdemo.kafka;
+
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.internals.DefaultPartitioner;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author wangmingxin03
+ * Created on 2021-12-16
+ */
+@Slf4j
+public class SimplePartitionerKafkaProducer {
+    private static final String SERVER_LIST = "39.97.47.254:9092,39.97.47.254:9093";
+
+    private static final String TOPIC_NAME = "xinye";
+
+    public static void main(String[] args) throws Exception {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, SERVER_LIST);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // 指定分区策略 partitioner.class，默认的 DefaultPartitioner 是hash的
+        properties.setProperty(ProducerConfig.PARTITIONER_CLASS_CONFIG, DefaultPartitioner.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "888");
+        Future<RecordMetadata> result = producer.send(record);
+
+        RecordMetadata recordMetadata = result.get();
+        int partition = recordMetadata.partition();
+        long offset = recordMetadata.offset();
+        log.info("produce msg | partition:{}, offset:{}", partition, offset);
+    }
+}
