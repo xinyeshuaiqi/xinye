508604bb2a1e5b84b3812f73fb935f7e7039afe3
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-02-10 19:53:31 +0800

    CircularQueue

---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/CircularQueue.java
@@ -0,0 +1,55 @@
+package pers.wmx.springbootfreemarkerdemo.alg;
+
+/**
+ * 循环队列
+ *
+ * @author wangmingxin03
+ * Created on 2022-02-10
+ */
+public class CircularQueue {
+    private String[] items;
+
+    private int n = 0;
+
+    private int head = 0;
+
+    private int tail = 0;
+
+    /**
+     * 初始化队列，head = tail = 0, 队列为空
+     **/
+    public CircularQueue(int capacity) {
+        items = new String[capacity];
+        n = capacity;
+    }
+
+    public boolean enqueue(String item) {
+        if ((tail + 1) % n == head) {
+            // 队列满了，循环队列尾追上了队列头
+            return false;
+        }
+
+        items[tail] = item;
+        tail = (tail + 1) % n;
+        return true;
+    }
+
+    public String dequeue() {
+        if (head == tail) {
+            // 空队列
+            return null;
+        }
+
+        String item = items[head];
+        head = (head + 1) % n;
+        return item;
+    }
+
+    public int getHead() {
+        return head;
+    }
+
+    public int getTail() {
+        return tail;
+    }
+}
---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/CircularQueueTest.java
@@ -0,0 +1,33 @@
+package pers.wmx.springbootfreemarkerdemo.alg;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author wangmingxin03
+ * Created on 2022-02-10
+ */
+@Slf4j
+public class CircularQueueTest {
+    public static void main(String[] args) {
+        CircularQueue queue = new CircularQueue(5);
+        queue.enqueue("a");
+        queue.enqueue("b");
+        queue.enqueue("c");
+        queue.enqueue("d");
+        queue.enqueue("e");
+        queue.enqueue("f");
+        log.info(queue.getHead() + "-" + queue.getTail());
+
+        log.info(queue.dequeue());
+        log.info(queue.dequeue());
+        log.info(queue.dequeue());
+
+        log.info(queue.getHead() + "-" + queue.getTail());
+
+        queue.enqueue("g");
+        queue.enqueue("h");
+        queue.enqueue("i");
+
+        log.info(queue.getHead() + "-" + queue.getTail());
+    }
+}
