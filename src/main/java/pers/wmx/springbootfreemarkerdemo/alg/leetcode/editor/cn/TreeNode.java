f6f5cba40c69e925f46607068d29518289c4db70
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-03-02 15:48:33 +0800

    tree node

---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,18 @@
+package pers.wmx.springbootfreemarkerdemo.alg.leetcode.editor.cn;
+
+/**
+ * @author wangmingxin03
+ * Created on 2022-03-02
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+}
