22428213b2074cf79f0a0e720483d17cfe851cd5
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-03-04 11:15:08 +0800

    MajorityElement

---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/MajorityElement.java
@@ -0,0 +1,55 @@
+package pers.wmx.springbootfreemarkerdemo.alg.leetcode.editor.cn;
+//给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
+//
+// 你可以假设数组是非空的，并且给定的数组总是存在多数元素。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：[3,2,3]
+//输出：3 
+//
+// 示例 2： 
+//
+// 
+//输入：[2,2,1,1,1,2,2]
+//输出：2
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 尝试设计时间复杂度为 O(n)、空间复杂度为 O(1) 的算法解决此问题。 
+// 
+// Related Topics 数组 哈希表 分治 计数 排序 
+// 👍 1327 👎 0
+
+
+import java.util.Arrays;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        Solution solution = new MajorityElement().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = (low + high) / 2;
+
+        // 出现超过一半的数一定是排好序中间的那个数
+        return nums[mid];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
+
