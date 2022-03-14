2072430d87391c734ed1d3c8d5656aac70a32a0d
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-03-14 16:21:30 +0800

    maxDepth

--- src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/MaximumDepthOfBinaryTree.java
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/MaximumDepthOfBinaryTree.java
@@ -39,9 +39,17 @@ public class MaximumDepthOfBinaryTree {
  *     }
  * }
  */
+
+// 104
 class Solution {
     public int maxDepth(TreeNode root) {
-        return 0;
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth,rightDepth) + 1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
