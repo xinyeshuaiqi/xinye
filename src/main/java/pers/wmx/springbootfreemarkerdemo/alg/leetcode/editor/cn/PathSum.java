29e7129b9c52c26c9e6d4e9ec2a6ec768b383c69
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-03-14 16:48:21 +0800

    hasPathSum

--- src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/PathSum.java
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/PathSum.java
@@ -1,4 +1,5 @@
-package pers.wmx.springbootfreemarkerdemo.alg.leetcode.editor.cn;//给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和
+package pers.wmx.springbootfreemarkerdemo.alg.leetcode.editor.cn;
+//给你二叉树的根节点 root 和一个表示目标和的整数 targetSum 。判断该树中是否存在 根节点到叶子节点 的路径，这条路径上所有节点值相加等于目标和
 // targetSum 。如果存在，返回 true ；否则，返回 false 。 
 //
 // 叶子节点 是指没有子节点的节点。 
@@ -65,9 +66,21 @@ public class PathSum {
  *     }
  * }
  */
+// 112
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return true;
+        if (root == null) {
+            return false;
+        }
+
+        // 走到最后啦！！！
+        // 注意递归的中断条件！！！
+        if (root.left == null && root.right == null) {
+            return  root.val == targetSum;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val)
+                || hasPathSum(root.right, targetSum - root.val);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
