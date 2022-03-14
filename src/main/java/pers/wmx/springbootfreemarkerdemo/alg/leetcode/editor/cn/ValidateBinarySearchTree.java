82b6a33ce4cdd7eaf671b0a5d0e7e9ce49e7dc24
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-03-14 16:31:56 +0800

    isValidBST

--- src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/ValidateBinarySearchTree.java
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/ValidateBinarySearchTree.java
@@ -58,9 +58,23 @@ public class ValidateBinarySearchTree {
  *     }
  * }
  */
+// 98
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return true;
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean isValidBST(TreeNode root, long minValue, long maxValue) {
+        if (root == null) {
+            return true;
+        }
+
+        if (root.val <= minValue || root.val >= maxValue) {
+            return false;
+        }
+
+        return isValidBST(root.left, minValue, root.val)
+                && isValidBST(root.right, root.val, maxValue);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
