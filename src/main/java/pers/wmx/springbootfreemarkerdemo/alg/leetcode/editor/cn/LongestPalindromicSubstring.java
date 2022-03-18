cebec74db3a0c64ac273c3760bb19024fe704b48
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-03-18 11:40:45 +0800

    update

---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/LongestPalindromicSubstring.java
@@ -0,0 +1,46 @@
+package pers.wmx.springbootfreemarkerdemo.alg.leetcode.editor.cn;//给你一个字符串 s，找到 s 中最长的回文子串。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "babad"
+//输出："bab"
+//解释："aba" 同样是符合题意的答案。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "cbbd"
+//输出："bb"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 仅由数字和英文字母组成 
+// 
+// Related Topics 字符串 动态规划 
+// 👍 4882 👎 0
+
+
+public class LongestPalindromicSubstring {
+    public static void main(String[] args) {
+        Solution solution = new LongestPalindromicSubstring().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String longestPalindrome(String s) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
+
