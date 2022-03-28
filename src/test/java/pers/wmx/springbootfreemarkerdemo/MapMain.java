fbc03b7a006a05144fc5057955d2c93b021b55dc
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-03-28 16:51:20 +0800

    map

--- src/test/java/pers/wmx/springbootfreemarkerdemo/MapMain.java
+++ src/test/java/pers/wmx/springbootfreemarkerdemo/MapMain.java
@@ -15,8 +15,16 @@ public class MapMain {
     public static void main(String[] args) {
         List<Integer> list = ImmutableList.of(1,2,3,1);
 
-        Map<Integer, Integer> collect = list.stream()
+        Map<Integer, Integer> map = list.stream()
                 .collect(Collectors.toMap(Function.identity(), Function.identity(), (a, b) -> a));
-        System.out.println(collect);
+        System.out.println(map);
+
+        map.computeIfAbsent(5, key -> 555);
+        System.out.println(map);
+
+        // 如果 key 对应的 value 不存在，则返回该 null。
+        // 如果存在，则返回通过 remappingFunction 重新计算后的值。
+        map.computeIfPresent(6, (key,value) -> value + value * 3);
+        System.out.println(map);
     }
 }
