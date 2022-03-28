5a498484cd54da4c3ca1564b8ddaaf28947114cc
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-03-28 16:01:46 +0800

    removeNthFromEnd

---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,95 @@
+package pers.wmx.springbootfreemarkerdemo.alg.leetcode.editor.cn;//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+//
+// 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+// Related Topics 链表 双指针 
+// 👍 1914 👎 0
+
+
+import pers.wmx.springbootfreemarkerdemo.alg.leetcode.editor.cn.ListNode;
+
+public class RemoveNthNodeFromEndOfList {
+    public static void main(String[] args) {
+        Solution solution = new RemoveNthNodeFromEndOfList().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode result = new ListNode();
+        result.next = head;
+
+        ListNode node = findFromEnd(result, n + 1);
+        node.next = node.next.next;
+
+        return result.next;
+    }
+
+    // 找到倒数第k个节点 （第n个节点的前一个节点）
+    // size = 5, n = 2, k =3
+    private ListNode findFromEnd(ListNode head, int k) {
+        ListNode p1 = head;
+        for (int i = 0; i < k; i++) {
+            p1 = p1.next;
+        }
+
+        ListNode p2 = head;
+
+        // p1、p2一起走
+        while (p1 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return p2;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
+
