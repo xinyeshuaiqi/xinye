31d576c27e410b8291c8882b0405b831c63ff8d7
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-03-30 15:51:32 +0800

    mod

--- src/main/java/pers/wmx/springbootfreemarkerdemo/Main.java
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/Main.java
@@ -6,6 +6,11 @@ package pers.wmx.springbootfreemarkerdemo;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println(0 % 5);
+        System.out.println(1 % 5);
+        System.out.println(2 % 5);
+        System.out.println(3 % 5);
+        System.out.println(4 % 5);
 
     }
 }
