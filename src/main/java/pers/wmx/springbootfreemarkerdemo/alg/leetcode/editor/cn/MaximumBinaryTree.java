bb389711fdf469710c3fea2bd8e68431f73631e4
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-04-07 10:52:34 +0800

    constructMaximumBinaryTree

---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/MaximumBinaryTree.java
@@ -0,0 +1,106 @@
+package pers.wmx.springbootfreemarkerdemo.alg.leetcode.editor.cn;//给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建:
+//
+// 
+// 创建一个根节点，其值为 nums 中的最大值。 
+// 递归地在最大值 左边 的 子数组前缀上 构建左子树。 
+// 递归地在最大值 右边 的 子数组后缀上 构建右子树。 
+// 
+//
+// 返回 nums 构建的 最大二叉树 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [3,2,1,6,0,5]
+//输出：[6,3,5,null,2,0,null,null,1]
+//解释：递归调用如下所示：
+//- [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+//    - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+//        - 空数组，无子节点。
+//        - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+//            - 空数组，无子节点。
+//            - 只有一个元素，所以子节点是一个值为 1 的节点。
+//    - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+//        - 只有一个元素，所以子节点是一个值为 0 的节点。
+//        - 空数组，无子节点。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,1]
+//输出：[3,null,2,null,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 1000 
+// 0 <= nums[i] <= 1000 
+// nums 中的所有整数 互不相同 
+// 
+// Related Topics 栈 树 数组 分治 二叉树 单调栈 
+// 👍 400 👎 0
+
+
+import pers.wmx.springbootfreemarkerdemo.alg.leetcode.editor.cn.TreeNode;
+
+public class MaximumBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new MaximumBinaryTree().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int low, int high) {
+        if (low > high) {
+            return null;
+        }
+
+        int maxVal = Integer.MIN_VALUE;
+        int index = 0;
+        for (int i = low; i <= high; i++) {
+            if (nums[i] > maxVal) {
+                maxVal = nums[i];
+                index = i;
+            }
+        }
+
+        // 以最大值构建当前根结点
+        TreeNode root = new TreeNode(maxVal);
+        root.left = build(nums, low, index - 1);
+        root.right = build(nums, index + 1, high);
+        return root;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
+
