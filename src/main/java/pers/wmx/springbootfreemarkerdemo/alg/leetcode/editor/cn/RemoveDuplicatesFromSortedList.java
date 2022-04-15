eb88500db5ebb473a3f280bfeb00542d5281507a
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-04-15 11:21:17 +0800

    deleteDuplicates

--- src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/RemoveDuplicatesFromSortedList.java
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/RemoveDuplicatesFromSortedList.java
@@ -46,18 +46,38 @@ public class RemoveDuplicatesFromSortedList {
  * }
  */
 class Solution {
+//    public ListNode deleteDuplicates(ListNode head) {
+//        if (head == null) {
+//            return head;
+//        }
+//
+//        ListNode cur = head;
+//        while (cur.next != null) {
+//            if (cur.val == cur.next.val) {
+//                cur.next = cur.next.next;
+//            } else {
+//                cur = cur.next;
+//            }
+//        }
+//
+//        return head;
+//    }
+
     public ListNode deleteDuplicates(ListNode head) {
         if (head == null) {
-            return head;
+            return null;
         }
 
         ListNode cur = head;
         while (cur.next != null) {
-            if (cur.val == cur.next.val) {
-                cur.next = cur.next.next;
+            ListNode next = cur.next;
+
+            if (cur.val == next.val) {
+                cur.next = next.next;
             } else {
                 cur = cur.next;
             }
+
         }
 
         return head;
