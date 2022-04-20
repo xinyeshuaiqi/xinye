2459e6976d7a7462994f34b720c8e31846ff1b7e
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-04-20 16:34:43 +0800

    fib

--- src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/FibonacciNumber.java
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/FibonacciNumber.java
@@ -76,20 +76,36 @@ class Solution {
 //        }
 
 
-        int fib(int n) {
-            // 备忘录全初始化为 0
-            int[] memo = new int[n + 1];
-            // 进行带备忘录的递归
-            return helper(memo, n);
-        }
+//        int fib(int n) {
+//            // 备忘录全初始化为 0
+//            int[] memo = new int[n + 1];
+//            // 进行带备忘录的递归
+//            return helper(memo, n);
+//        }
+//
+//        int helper(int[] memo, int n) {
+//            // base case
+//            if (n == 0 || n == 1) return n;
+//            // 已经计算过，不用再计算了
+//            if (memo[n] != 0) return memo[n];
+//            memo[n] = helper(memo, n - 1) + helper(memo, n - 2);
+//            return memo[n];
+//        }
 
-        int helper(int[] memo, int n) {
-            // base case
-            if (n == 0 || n == 1) return n;
-            // 已经计算过，不用再计算了
-            if (memo[n] != 0) return memo[n];
-            memo[n] = helper(memo, n - 1) + helper(memo, n - 2);
-            return memo[n];
+        int fib(int n) {
+            if (n == 0 || n == 1) {
+                return n;
+            }
+
+            int []dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = 1;
+
+            for (int i = 2; i <= n; i++) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[n];
         }
 
     }
