da7ee7566c8d4f7b7134e517e818146e77e17e48
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-04-21 21:31:57 +0800

    SubarraySumEqualsK

---
+++ src/main/java/pers/wmx/springbootfreemarkerdemo/alg/leetcode/editor/cn/SubarraySumEqualsK.java
@@ -0,0 +1,48 @@
+package pers.wmx.springbootfreemarkerdemo.alg.leetcode.editor.cn;//给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,1,1], k = 2
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,2,3], k = 3
+//输出：2
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 2 * 104 
+// -1000 <= nums[i] <= 1000 
+// -107 <= k <= 107 
+// 
+// Related Topics 数组 哈希表 前缀和 
+// 👍 1445 👎 0
+
+
+public class SubarraySumEqualsK {
+    public static void main(String[] args) {
+        Solution solution = new SubarraySumEqualsK().new Solution();
+        System.out.println("Hello world");
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+
+
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+} 
+
