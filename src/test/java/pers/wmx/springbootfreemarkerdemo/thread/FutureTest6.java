bffc91ab80f6c5ac99782c85da4414a2b9ca64ae
wangmingxin03 <wangmingxin03@kuaishou.com>
2022-10-18 15:09:02 +0800

    up

--- src/test/java/pers/wmx/springbootfreemarkerdemo/thread/FutureTest6.java
+++ src/test/java/pers/wmx/springbootfreemarkerdemo/thread/FutureTest6.java
@@ -2,8 +2,6 @@ package pers.wmx.springbootfreemarkerdemo.thread;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
