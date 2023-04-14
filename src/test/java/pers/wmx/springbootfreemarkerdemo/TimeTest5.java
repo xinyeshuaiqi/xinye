c1c0fb0b3fc1d706a206de647341a2446e89d1da
wangmingxin03 <wangmingxin03@kuaishou.com>
2023-04-14 18:28:33 +0800

    update

---
+++ src/test/java/pers/wmx/springbootfreemarkerdemo/TimeTest5.java
@@ -0,0 +1,25 @@
+package pers.wmx.springbootfreemarkerdemo;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author wangmingxin03
+ * Created on 2022-09-02
+ */
+public class TimeTest5 {
+    private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public static void main(String[] args) {
+
+        long timeStamp = System.currentTimeMillis();
+        LocalDateTime localDateTime =
+                LocalDateTime.ofInstant(Instant.ofEpochMilli(timeStamp), ZoneId.systemDefault());
+
+        System.out.println(localDateTime.format(FMT));
+
+    }
+
+}
